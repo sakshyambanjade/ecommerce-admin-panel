5ecb616fb10f209ffd0a4c78f7538074524f0dc9
SAKSHYAM BANJADE <sakshyambanjade@gmail.com>
2023-07-04 18:53:41 +0545

    made proper rotingy of login/register

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -23,7 +23,7 @@ export  class Login extends Component {
         <a className="block text-blue-600" href="/">
           <span className="sr-only">Home</span>
           <svg
-            className="h-8 sm:h-10"
+            className="h-8 sm:h-20"
             viewBox="0 0 28 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -36,9 +36,9 @@ export  class Login extends Component {
         </a>
 
         <h1
-          className="mt-6 text-2xl font-bold text-gray-900 dark:text-white sm:text-3xl md:text-4xl"
+          className="mt-6 text-2xl font-bold text-gray-900 dark:text-white sm:text-3xl md:text-4xl "
         >
-          Welcome to Squid 🦑
+          Welcome to SKB SHOP
         </h1>
 
         <p className="mt-4 leading-relaxed text-gray-500 dark:text-gray-400">
@@ -46,56 +46,25 @@ export  class Login extends Component {
           dolorum aliquam, quibusdam aperiam voluptatum.
         </p>
 
-        <form action="#" className="mt-8 grid grid-cols-6 gap-6">
-          <div className="col-span-6 sm:col-span-3">
-            <label
-              htmlFor="FirstName"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              First Name
-            </label>
-
-            <input
-              type="text"
-              id="FirstName"
-              name="first_name"
-              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
-            />
-          </div>
-
-          <div className="col-span-6 sm:col-span-3">
-            <label
-              htmlFor="LastName"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              Last Name
-            </label>
-
-            <input
-              type="text"
-              id="LastName"
-              name="last_name"
-              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
-            />
-          </div>
-
+        <form action="#" className="mt-8 grid grid-cols-5 gap-8">
+          
           <div className="col-span-6">
             <label
               htmlFor="Email"
               className="block text-sm font-medium text-gray-700 dark:text-gray-200"
             >
-              Email
+              Email/Username
             </label>
 
             <input
               type="email"
               id="Email"
               name="email"
-              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
+              className="mt-5 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 h-10"
             />
           </div>
 
-          <div className="col-span-6 sm:col-span-3">
+          <div className="col-span-6 ">
             <label
               htmlFor="Password"
               className="block text-sm font-medium text-gray-700 dark:text-gray-200"
@@ -107,66 +76,21 @@ export  class Login extends Component {
               type="password"
               id="Password"
               name="password"
-              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
+              className="mt-5 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 h-10"
             />
-          </div>
-
-          <div className="col-span-6 sm:col-span-3">
-            <label
-              htmlFor="PasswordConfirmation"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              Password Confirmation
-            </label>
-
-            <input
-              type="password"
-              id="PasswordConfirmation"
-              name="password_confirmation"
-              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
-            />
-          </div>
-
-          <div className="col-span-6">
-            <label htmlFor="MarketingAccept" className="flex gap-4">
-              <input
-                type="checkbox"
-                id="MarketingAccept"
-                name="marketing_accept"
-                className="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:focus:ring-offset-gray-900"
-              />
-
-              <span className="text-sm text-gray-700 dark:text-gray-200">
-                I want to receive emails about events, product updates and
-                company announcements.
-              </span>
-            </label>
-          </div>
-
-          <div className="col-span-6">
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              By creating an account, you agree to our
-              <a href="#" className="text-gray-700 underline dark:text-gray-200">
-                terms and conditions
-              </a>
-              and
-              <a href="#" className="text-gray-700 underline dark:text-gray-200">
-                privacy policy </a
-              >.
-            </p>
           </div>
 
           <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
             <button
               className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white"
             >
-              Create an account
-            </button>
+           Login            
+           </button>
 
             <p className="mt-4 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-              Already have an account?
-              <a href="#" className="text-gray-700 underline dark:text-gray-200"
-                >Log in</a
+              Need an account?
+              <a href="./register" className="text-gray-700 underline dark:text-gray-200"
+                >Signup</a
               >.
             </p>
           </div>
